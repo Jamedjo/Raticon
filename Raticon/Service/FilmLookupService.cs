256a964385d1a38673640f9d5935353e094c70c7
James EdJo <jamedjo@gmail.com>
2014-05-15 16:58:43 +0100

    Added film lookup service for missing imdb ids

---
+++ Raticon/Service/FilmLookupService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using Raticon.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raticon.Service
+{
+    public class FilmLookupService
+    {
+        public RatingResult[] Search(string title, IHttpService httpService = null)
+        {
+            string data = (httpService ?? new HttpService()).Get(@"http://www.myapifilms.com/title?limit=10&title=" + title);
+            JArray objects = JArray.Parse(data);
+            return objects.Select(o => parseOne(o)).ToArray<RatingResult>();
+        }
+
+        private RatingResult parseOne(JToken o)
+        {
+            return new RatingResult
+            {
+                Title = o["title"].ToString(),
+                Year = o["year"].ToString(),
+                Rating = o["rating"].ToString(),
+                Poster = o["urlPoster"].ToString()
+            };
+        }
+
+    }
+}
---
+++ RaticonTest/FilmLookupServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Raticon.Service;
+
+namespace RaticonTest
+{
+    [TestClass]
+    public class FilmLookupServiceTest
+    {
+        [TestMethod]
+        public void It_should_return_multiple_results_for_ambiguous_titles()
+        {
+            var results = new FilmLookupService().Search("Italian Job");
+            Assert.IsTrue(results.Length >= 2);
+        }
+    }
+}
