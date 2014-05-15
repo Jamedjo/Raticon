b2f78f732a8cb941b0d9f8d186fce0a5eddaae22
James EdJo <jamedjo@gmail.com>
2014-05-15 09:46:30 +0100

    Began using ImageMagick COM object instead of shell script

---
+++ Raticon/Service/ConvertService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raticon.Service
+{
+    public class ConvertService
+    {
+        /// <summary>
+        /// Uses ImageMagick COM object to convert images
+        /// </summary>
+        /// <param name="workingDirectory"></param>
+        /// <param name="arguments">Array of arguments to pass to ImageMagick</param>
+        /// <param name="fileName">Original name of file to be processed</param>
+        public void Convert(string workingDirectory, List<string> arguments, string fileName)
+        {
+            ImageMagickObject.MagickImage img = new ImageMagickObject.MagickImage();
+            arguments.Add(fileName);
+            object[] args = arguments.ToArray<object>();
+            object result = img.Convert(args);
+        }
+    }
+}
