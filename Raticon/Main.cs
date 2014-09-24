5df0c207a961c0a1ba02e6552b9d5578484c9b6f
James EdJo <jamedjo@gmail.com>
2014-09-24 13:31:57 +0100

    App can be run as a command line app.

---
+++ Raticon/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Raticon
+{
+    public class EntryPoint
+    {
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool FreeConsole();
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern bool AttachConsole(int dwProcessId);
+ 
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public static void Main(string[] args)
+        {
+
+            if (!AttachConsole(-1))
+            {
+                Raticon.App app = new Raticon.App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            else
+            {
+                Console.WriteLine("Hi there");
+
+                FreeConsole();
+            }
+        }
+
+    }
+}
