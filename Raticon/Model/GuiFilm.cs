c42460db242abeb621fc36eed56fd342249c33bb
James EdJo <jamedjo@gmail.com>
2014-09-26 21:50:27 +0100

    Moved GuiFilm into its own file.

---
+++ Raticon/Model/GuiFilm.cs
@@ -0,0 +1,93 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Raticon.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Raticon.Model
+{
+    public class GuiFilm : CachedFilm, INotifyPropertyChanged
+    {
+        public GuiFilm(string path, IFileSystem fileSystem = null) : base(path, fileSystem, new GuiResultPickerService(Application.Current.MainWindow))
+        {
+            FetchData();
+        }
+
+        public void FetchData()
+        {
+            setImdbFromService(() => updateRatingResultCache());
+        }
+
+        public bool IsLoading { get { return (idLookupInvoked && !idLookupComplete) || (ratingLookupInvoked && !ratingLookupComplete); } }
+
+        private bool ratingLookupInvoked = false;
+        private bool ratingLookupComplete = false;
+        protected override void updateRatingResultCache()
+        {
+            if (!ratingLookupInvoked)
+            {
+                ratingLookupInvoked = true;
+                Messenger.Default.Send(this, "FilmLoadingChanged");
+
+                Task.Factory.StartNew<RatingResult>(() => getRatingSafe()).ContinueWith((t) =>
+                {
+                    ratingResultCache = t.Result;
+                    OnRatingChanged();
+                    ratingLookupComplete = true;
+                });
+            }
+        }
+
+        public string Icon
+        {
+            get
+            {
+                string iconPath = PathTo("folder.ico");
+                return (fileSystem.File.Exists(iconPath)) ? iconPath : null;
+            }
+        }
+        public void IconUpdated() { OnPropertyChanged("Icon"); }
+
+        private bool idLookupInvoked = false;
+        private bool idLookupComplete = false;
+        protected override void setImdbFromService(Action onComplete)
+        {
+            if (!idLookupInvoked)
+            {
+                idLookupInvoked = true;
+                Messenger.Default.Send(this, "FilmLoadingChanged");
+                var task = idLookupService.LookupAsync(FolderName, (lookup) => resultPicker.Pick(lookup));
+                task.ContinueWith((t) =>
+                {
+                    imdbIdCache = t.Result;
+                    OnRatingChanged();
+                    idLookupComplete = true;
+                    onComplete();
+                });
+            }
+        }
+
+        private void OnRatingChanged()
+        {
+            Messenger.Default.Send(this, "FilmLoadingChanged");
+            OnPropertyChanged("IsLoading");
+            OnPropertyChanged("Title");
+            OnPropertyChanged("Rating");
+            OnPropertyChanged("Year");
+            OnPropertyChanged("Poster");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
